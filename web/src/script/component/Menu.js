5be5e8ab5d3e258e823c62e7ae36043618e3296e
gonzo <m165252@daum.net>
2019-11-17 12:12:52 +0900

    웹 기본 환경 세팅 #1

--- web/src/script/component/Menu.js
+++ web/src/script/component/Menu.js
@@ -1,11 +1,21 @@
+import React  , {useState} from 'react'
 
-
+const Info = () => {
+    return 'Test'
+};
 
 const getMenu = () => {
+
+    let test2 = Info();
+
     return (
         <div>
             <a href={'/Main'}>Main</a>
-            <a href={''}>User</a>
+            <a href={'/USer'}>User</a>
+            <a>{test2}</a>
         </div>
     )
 };
+
+export default getMenu
+
