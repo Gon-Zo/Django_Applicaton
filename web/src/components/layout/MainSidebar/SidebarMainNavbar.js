eecbf1feb16c1c243ab47fe7e30a97d13ed627a3
gonzo <m165252@daum.net>
2019-12-09 21:32:28 +0900

    웹 기본 환경 세팅 #1

--- web/src/components/layout/MainSidebar/SidebarMainNavbar.js
+++ web/src/components/layout/MainSidebar/SidebarMainNavbar.js
@@ -31,18 +31,20 @@ class SidebarMainNavbar extends React.Component {
             style={{ lineHeight: "25px" }}
           >
             <div className="d-table m-auto">
-              <img
-                id="main-logo"
-                className="d-inline-block align-top mr-1"
-                style={{ maxWidth: "25px" }}
-                src={require("../../../images/shards-dashboards-logo.svg")}
-                alt="Shards Dashboard"
-              />
-              {!hideLogoText && (
-                <span className="d-none d-md-inline ml-1">
-                  Shards Dashboard
-                </span>
-              )}
+              <span>HOME</span>
+              {/*<img*/}
+              {/*  id="main-logo"*/}
+              {/*  className="d-inline-block align-top mr-1"*/}
+              {/*  style={{ maxWidth: "25px" }}*/}
+              {/*  src={require("../../../images/shards-dashboards-logo.jpg")}*/}
+              {/*  alt="Shards Dashboard"*/}
+              {/*/>*/}
+              {/*{!hideLogoText && (*/}
+              {/*  <span className="d-none d-md-inline ml-1">*/}
+              {/*    Shards Dashboard*/}
+              {/*  </span>*/}
+              {/*)}*/}
+
             </div>
           </NavbarBrand>
           {/* eslint-disable-next-line */}
