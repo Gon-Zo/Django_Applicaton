997d62445a5a34d2490ef01a7991c95220092bae
gonzo <m165252@daum.net>
2019-12-21 10:49:02 +0900

    [fix] all web change

--- web/src/component/chart/ChartGroup.js
+++ web/src/component/chart/ChartGroup.js
@@ -5,29 +5,44 @@ import ChartCard3 from "./ChartCard3";
 import ListCard from "../list/ListCard";
 import ChartCard4 from "./ChartCard4";
 import ChartCard5 from "./ChartCard5";
+import ChartTest from "./ChartTest";
 
 
 export default () => {
     return (
         <div className="row tm-content-row">
 
-            <ChartCard1 title={"Latest Hits"}/>
+            {/*<div className="app-style-red app-slide-wrap">*/}
+            {/*    <ChartTest/>*/}
+            {/*</div>*/}
 
-            {/**/}
 
-            <ChartCard2 title={"Performance"}/>
+            <div>
 
-            {/**/}
+                <div className="app-card-sm col-sm-12 col-md-12 col-lg-6 col-xl-6 tm-block-col">
+                    <div className="tm-bg-primary-dark ">
+                        <div>
+                            <span>Test</span>
+                        </div>
+                    </div>
+                </div>
 
-            <ChartCard3 title={"Storage Information"}/>
+            </div>
 
-            {/**/}
 
-            <ChartCard4 title={"test1"}/>
+            {/*<ChartCard1 title={"Latest Hits"}/>*/}
 
-            <ChartCard5 title={"test2"}/>
+            {/*<ChartCard2 title={"Performance"}/>*/}
 
-            <ListCard title={"Notification List"}/>
+
+            {/*<ChartCard3 title={"Storage Information"}/>*/}
+
+
+            {/*<ChartCard4 title={"test1"}/>*/}
+
+            {/*<ChartCard5 title={"test2"}/>*/}
+
+            {/*<ListCard title={"Notification List"}/>*/}
 
             {/*  Table   */}
 
--- web/src/view/Dashboard.js
+++ web/src/view/Dashboard.js
@@ -7,12 +7,11 @@ import ChartGroup from "../component/chart/ChartGroup";
 export default () => {
     return (
         <div className="container">
-
-            <div className="row">
-                <div className="col">
-                    <p className="text-white mt-5 mb-5 text_start">Welcome back, <b>Admin</b></p>
-                </div>
-            </div>
+            {/*<div className="row">*/}
+            {/*    <div className="col">*/}
+            {/*        <p className="text-white mt-5 mb-5 text_start">Welcome back, <b>Admin</b></p>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
             {/* row */}
             <ChartGroup/>
         </div>
