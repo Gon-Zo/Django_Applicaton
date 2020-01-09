0df0c0e6afc1e2eb0d3398473faae8a3c79674fe
gonzo <m165252@daum.net>
2020-01-09 20:09:10 +0900

    global exception handler dev #23

--- api/venv/bin/App/App/util/ResponseFormattingMiddleware.py
+++ api/venv/bin/App/App/util/ResponseFormattingMiddleware.py
@@ -1,15 +1,12 @@
+# Request & Response Middleware
+
 import re
 from rest_framework.status import is_client_error, is_success
 from App.util.comm import ReqJSONRenderer
 from App.util.auth import __token_auth__
 
 
-# todo : Login Error checking to week
-
 class ResponseFormattingMiddleware:
-    """
-    Rest Framework 을 위한 전용 커스텀 미들웨어에 대해 response format 을 자동으로 세팅
-    """
     METHOD = ('GET', 'POST', 'PUT', 'PATCH', 'DELETE')
 
     def __init__(self, get_response):
@@ -28,50 +25,25 @@ class ResponseFormattingMiddleware:
         return response
 
     def process_response(self, request, response):
-        """
-        API_URLS 와 method 가 확인이 되면
-        response 로 들어온 data 형식에 맞추어
-        response_format 에 넣어준 후 response 반환
-        """
+
         path = request.path_info.lstrip('/')
         valid_urls = (url.match(path) for url in self.API_URLS)
 
         if request.method in self.METHOD and any(valid_urls):
-
-            status = response.status_code
-            status_yn = is_success(status)
-
-            if hasattr(response, 'data') and \
-                    getattr(response, 'data') is not None:
-                data = response.data
-                a = None
-                if status_yn:
-                    a = __render_format__(status_yn, data.pop('message'), data.pop('result'))
+            if __url_check__(request.path_info):
+                auth = request.META.get('HTTP_AUTHORIZATION')
+                if auth is None:
+                    return __render_response__(500, False, "[Mka Error] 500 ", "NONE AUTHORIZATION ERROR")
+                elif not __token_auth__(auth):
+                    return __render_response__(500, False, "[Mka Error] 500 ", "NOT JWT ERROR")
                 else:
-                    a = __render_format__(status_yn, "[Mka Error] " + str(status), data.get('detail'))
-
-            response.data = a
-            response.content = response.render().rendered_content
-            return ReqJSONRenderer(response.data, status=response.status_code)
-
-# if __url_check__(request.path_info):
-#     auth = request.META.get('HTTP_AUTHORIZATION')
-#     if auth is None:
-#         print("여기 !!")
-#         return __render_response__(500, False, "[Mka Error] 500 ", "NONE AUTHORIZATION ERROR")
-#     elif not __token_auth__(auth):
-#         print("여기 ?")
-#         return __render_response__(500, False, "[Mka Error] 500 ", "NOT JWT ERROR")
-#     else:
-#         print("여기는 토긐크크크")
-#         return self.__test__(self, request, response)
-# else:
-#     print("여기는 로그인")
-#     self.__test__(self, request, response)
+                    return __result_response__(request, response)
+            else:
+                return __result_response__(request, response)
 
 
+# ERROR message
 def getMkaErrorMessage(status):
-    print(str(status))
     if status == 404:
         return "Not Found"
     elif status == 500:
@@ -80,10 +52,12 @@ def getMkaErrorMessage(status):
         return "Null"
 
 
+# login url checking
 def __url_check__(url):
     return not str(url).__contains__('login')
 
 
+# json format
 def __render_format__(status_yn, message, result):
     response_format = {
         'success': status_yn,
@@ -93,5 +67,32 @@ def __render_format__(status_yn, message, result):
     return response_format
 
 
+# render to json
 def __render_response__(status, status_yn, message, result):
     return ReqJSONRenderer(__render_format__(status_yn, message, result), status=status)
+
+
+# render to json
+def __json_response__(format, status):
+    return ReqJSONRenderer(format, status=status)
+
+
+# success to auth and result to json response
+def __result_response__(request, response):
+    status = response.status_code
+    status_yn = is_success(response.status_code)
+    str_format = None
+    if hasattr(response, 'data') and \
+            getattr(response, 'data') is not None:
+        data = response.data
+        if is_success(response.status_code):
+            str_format = __render_format__(status_yn, data.pop('message'), data.pop('result'))
+        else:
+            str_format = __render_format__(status_yn, "[Mka Error] " + str(status), data.get('detail'))
+    else:
+        str_format = __render_format__(status_yn, "[Mka Error] " + str(status), getMkaErrorMessage(status))
+        return __json_response__(str_format, status=status)
+
+    response.data = str_format
+    response.content = response.render().rendered_content
+    return __json_response__(response.data, status)
