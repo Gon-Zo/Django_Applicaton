7e07c7f34cc97ee4c7c42d58ce9758e221f9241e
gonzo <m165252@daum.net>
2020-02-15 10:01:48 +0900

    Login View Development #6

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Main from "./assets/js/view/index";
-import AppSideBar from "./assets/js/component/AppSideBar";
+import AppLogin from "./assets/js/view/AppLogin";
 
 function App() {
     return (
-        <div>
-            <AppSideBar/>
-            <Main/>
-        </div>
+        <AppLogin/>
     );
 }
 
