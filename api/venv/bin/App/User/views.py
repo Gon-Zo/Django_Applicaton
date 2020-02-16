8ff4e397dcc4b9053787d760390f154edf646ec7
gonzo <m165252@daum.net>
2020-02-16 10:47:46 +0900

    admin api dev #38

--- api/venv/bin/App/User/views.py
+++ api/venv/bin/App/User/views.py
@@ -77,22 +77,22 @@ from User.models import User
 from App.util.comm import param_parser
 from App.util.auth import __encode_jwt__
 from rest_framework.decorators import api_view
-
+import json
 # 트랜잭션
 from django.db import transaction
+
 
 @api_view(['POST'])
 @transaction.atomic
 def user_login(request):
     method = request.method
     if method == 'POST':
-        data = param_parser(request.GET)
+        data = json.loads(request.body)
         user = User.objects.filter(**data)
         jwt = __encode_jwt__(user[0])
-        return Response(jwt,status=200)
+        return Response(jwt, status=200)
     else:
         return Response(status=404)
-
 
 # class Login(APIView):
 #
