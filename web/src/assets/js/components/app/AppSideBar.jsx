7c3f81d1d90429e340b2e24cf5f7596d5cf213e2
gonzo <m165252@daum.net>
2020-02-18 19:26:53 +0900

    사이드 메뉴 개발 #4

--- web/src/assets/js/components/app/AppSideBar.jsx
+++ web/src/assets/js/components/app/AppSideBar.jsx
@@ -3,19 +3,19 @@ import React from "react";
 
 export default () => {
     return (
-        <div class="bg-light border-right" id="sidebar-wrapper">
-            <div class="sidebar-heading">
+        <div className="bg-light border-right" id="sidebar-wrapper">
+            <div className="sidebar-heading">
                 <h5 id="appTitle">
                     Application Manager
                 </h5>
             </div>
-            <div class="list-group list-group-flush">
-                <a href="#" class="list-group-item list-group-item-action bg-light">Dashboard</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Shortcuts</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Overview</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Events</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>
-                <a href="#" class="list-group-item list-group-item-action bg-light">Status</a>
+            <div className="list-group list-group-flush">
+                <a href="#" className="list-group-item list-group-item-action bg-light">Dashboard</a>
+                <a href="#" className="list-group-item list-group-item-action bg-light">Shortcuts</a>
+                <a href="#" className="list-group-item list-group-item-action bg-light">Overview</a>
+                <a href="#" className="list-group-item list-group-item-action bg-light">Events</a>
+                <a href="#" className="list-group-item list-group-item-action bg-light">Profile</a>
+                <a href="#" className="list-group-item list-group-item-action bg-light">Status</a>
             </div>
         </div>
     )
