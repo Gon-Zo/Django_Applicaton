1dc64fee06c3fe3f8534e76566e492d18af51183
gonzo <m165252@daum.net>
2020-02-22 10:27:15 +0900

    사이드 메뉴 개발 #4

--- web/src/assets/js/components/app/AppSideBar.jsx
+++ web/src/assets/js/components/app/AppSideBar.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import {Col, Image, Row} from "react-bootstrap";
-// import {Col, Container, Row} from "react-bootstrap";
 import imageSrc from '../../../image/default.png'
-// import * as fa from "@fortawesome/free-solid-svg-icons";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faUser} from '@fortawesome/free-solid-svg-icons'
 
 export default () => {
     return (
         <div className="border-right" id="sidebar-wrapper">
             <div className="sidebar-heading sidebar-profile">
-                {/*<Link to="/">*/}
-                {/*    <h5 id="appTitle">*/}
-                {/*        Log*/}
-                {/*    </h5>*/}
-                {/*</Link>*/}
                 <Row>
                     <Col>
                         <Image width="150" height="150" src={imageSrc} className="user-profile"/>
@@ -31,12 +25,9 @@ export default () => {
                     </Col>
                 </Row>
             </div>
-            {/*<div>*/}
-            {/*    <span>TEST</span>*/}
-            {/*</div>*/}
             <div className="list-group list-group-flush">
                 <Link to="/user" className="list-group-item list-group-item-action bg-light">
-                    {/*<FontAwesomeIcon icon={fa.faUsers}/>*/}
+                    <FontAwesomeIcon icon={faUser}/>
                     User
                 </Link>
                 <Link to="/" className="list-group-item list-group-item-action bg-light">Dashboard</Link>
