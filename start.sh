0ce743c0e2207df946ceafdb28070cd1b8ea9829
gonzo <m165252@daum.net>
2020-03-19 19:23:46 +0900

    shell file

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 source api/venv/bin/activate
+#cd /Users/bagjeong-gyu/workspace/python-market-app/api/venv/bin
+#pip freeze > requirements.txt
+#pip install -r requirements.txt
 cd /Users/bagjeong-gyu/workspace/python-market-app/api/venv/bin/App
+#python manage.py makemigrations
+#python manage.py migrate
 python3 manage.py runserver
