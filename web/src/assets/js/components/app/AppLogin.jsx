f2a89ba5ef47f1338e9593015c36b7828a848413
gonzo <m165252@daum.net>
2020-03-24 22:52:51 +0900

    react hook api dev >> setting #45

--- web/src/assets/js/components/app/AppLogin.jsx
+++ web/src/assets/js/components/app/AppLogin.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React ,{ useState} from "react";
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
 
 function LoginView(props) {
     return (
@@ -19,3 +21,29 @@ let catchLogin = (data) =>{
 }
 
 export {catchLogin}
+
+
+function AlertDismissible() {
+    const [show, setShow] = useState(true);
+
+    return (
+        <>
+            <Alert show={show} variant="success">
+                <Alert.Heading>How's it going?!</Alert.Heading>
+                <p>
+                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
+                    lacinia odio sem nec elit. Cras mattis consectetur purus sit amet
+                    fermentum.
+                </p>
+                <hr />
+                <div className="d-flex justify-content-end">
+                    <Button onClick={() => setShow(false)} variant="outline-success">
+                        Close me ya'll!
+                    </Button>
+                </div>
+            </Alert>
+
+            {!show && <Button onClick={() => setShow(true)}>Show Alert</Button>}
+        </>
+    );
+}
