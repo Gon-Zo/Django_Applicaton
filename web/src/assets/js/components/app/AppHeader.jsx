7123ad265d9842302b49146275629eb6976ce807
gonzo <m165252@daum.net>
2020-04-01 23:15:43 +0900

    style :: dark theme -> light theme #47

--- web/src/assets/js/components/app/AppHeader.jsx
+++ web/src/assets/js/components/app/AppHeader.jsx
@@ -1,22 +1,13 @@
 import React, {useState , useEffect} from "react";
 import {$httpLogout} from "../../modules/api/user";
 import {useDispatch, useSelector} from "react-redux";
-import {Nav, Navbar, Dropdown, Form, Button} from "react-bootstrap";
-import SplitButton from "react-bootstrap/SplitButton";
+import {Nav, Navbar, Dropdown, Form, Button, ButtonGroup} from "react-bootstrap";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import * as i from '@fortawesome/free-solid-svg-icons'
 import {changeTheme} from "../../modules/reducer/user";
-
-function ThemeIcon(props) {
-    let isTheme = props.isTheme
-    let themeStr = isTheme ? "다크 모드" : "라이트 모드"
-    return (
-        <>
-            <FontAwesomeIcon className="theme-icon " icon={i.faAdjust}/>
-            <span className="ml-2">{themeStr}</span>
-        </>
-    )
-}
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+import { useHistory } from 'react-router-dom';
 
 export default () => {
 
@@ -27,46 +18,83 @@ export default () => {
         $httpLogout(dispatch)
     };
 
+    let history = useHistory();
+
+    let _goToHref = ()=>{
+        history.push('/myinfo')
+    }
+
+    let theme = initUser.isTheme ? "primary" : "dark";
 
     return (
         // <Navbar bg="dark" variant="dark">
         <Navbar className="surface-bg">
             <Navbar.Brand href="#/home">
-                <span className="main-ft">
+                <span className="title-ft">
                 Application Manager
                 </span>
             </Navbar.Brand>
             {/*app title*/}
 
             <Navbar.Collapse className="justify-content-end">
-                <i className="fab fa-cloudscale"></i>
-                <SplitButton
-                    key={"down"}
-                    id={`dropdown-button-drop-down`}
-                    drop={"down"}
-                    variant={initUser.isTheme ? "light" : "dark"}
-                    title={<FontAwesomeIcon icon={i.faCogs}></FontAwesomeIcon>} >
+                <ButtonGroup>
+                    <ThemeIcon dispatch={dispatch} theme={theme}/>
 
-                    <Dropdown.Item eventKey="1"  onClick={()=>dispatch(changeTheme())}  >
-                        <ThemeIcon isTheme={initUser.isTheme}/>
-                    </Dropdown.Item>
-                    {/*<i className="far fa-address-card"></i>*/}
-                    <Dropdown.Item eventKey="2" href="/#/myinfo">
-                        <FontAwesomeIcon icon={i.faAddressCard}/>
-                        <span className="ml-2">
-                        내정보
-                        </span>
-                    </Dropdown.Item>
-                    <Dropdown.Item eventKey="3" onClick={() => $offUser()}>
-                        <FontAwesomeIcon icon={i.faSignOutAlt}/>
-                        <span className="ml-2">
-                        로그아웃
-                       </span> 
-                    </Dropdown.Item>
+                    <HeadButton
+                        icon={i.faAddressCard}
+                        theme={theme}
+                        onClick={_goToHref}
+                        tooltip={'내정보'}/>
 
-                </SplitButton>
+                    <HeadButton
+                        icon={i.faSignOutAlt}
+                        theme={theme}
+                        onClick={$offUser}
+                        tooltip={'로그아웃'}/>
 
+                </ButtonGroup>
             </Navbar.Collapse>
         </Navbar>
+    )
+}
+
+function HeadButton(props) {
+    let icon = props.icon
+    let theme = props.theme
+    let _onClick = props.onClick
+    let tooltip = props.tooltip
+    let className = props.className
+
+    return(
+        <OverlayTrigger
+            key={'bottom'}
+            placement={'bottom'}
+            overlay={
+                <Tooltip>
+                    {tooltip}
+                </Tooltip>
+            }>
+            <Button variant={theme} onClick={_onClick}>
+                <FontAwesomeIcon className={className} icon={icon}/>
+            </Button>
+        </OverlayTrigger>
+    )
+}
+
+function ThemeIcon(props) {
+    let dispatch = props.dispatch
+    let theme = props.theme
+    let _changeTheme = ()=>{
+        dispatch(changeTheme())
+    }
+    let themeStr = theme =='dark' ? "라이트모드" : "다크모드"
+    return (
+        <HeadButton
+            icon={i.faAdjust}
+            theme={theme}
+            className={"theme-icon"}
+            onClick={_changeTheme}
+            tooltip={themeStr}>
+        </HeadButton>
     )
 }
