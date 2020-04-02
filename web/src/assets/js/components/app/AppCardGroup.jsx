00db6a19ffff09e8dd50fffeabcd2a12e0db7abf
gonzo <m165252@daum.net>
2020-04-02 21:03:42 +0900

    style :: dark theme -> light theme #47

--- web/src/assets/js/components/app/AppCardGroup.jsx
+++ web/src/assets/js/components/app/AppCardGroup.jsx
@@ -3,13 +3,13 @@ import React from "react";
 export default () => {
     return (
         <div className="card-group">
-            <div className="card"></div>
-            <div className="card card-1 surface-bg"></div>
-            <div className="card card-2 surface-bg"></div>
-            <div className="card card-3 surface-bg"></div>
-            <div className="card card-4 surface-bg"></div>
-            <div className="card card-5 surface-bg"></div>
-            <div className="card card-test surface-bg"></div>
+            <div className="card card-bg"></div>
+            <div className="card card-1 card-bg"></div>
+            <div className="card card-2 card-bg"></div>
+            <div className="card card-3 card-bg"></div>
+            <div className="card card-4 card-bg"></div>
+            <div className="card card-5 card-bg"></div>
+            <div className="card card-test card-bg"></div>
         </div>
     )
 }
