d8a246d6440955dcf0d304c1279bffdd82bd384b
gonzo <m165252@daum.net>
2020-04-27 21:16:21 +0900

    web project remove

--- web/src/assets/js/modules/api/order.js
+++
@@ -1,9 +0,0 @@
-import {setFrom, setTo} from "../reducer/order";
-
-export function setDay(dispatch , payload) {
-    if(payload.type == 'F'){
-       dispatch(setFrom(payload.day))
-    }else{
-       dispatch(setTo(payload.day))
-    }
-}
